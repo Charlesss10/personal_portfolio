fd1732d297a60dbe11b395debfc8b46e39558029
Eboson Charles Ikenna <ebosonc@gmail.com>
2025-05-01 21:19:42 +0200

    13th Commit: Update Dependencies & Hero Update

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -15,8 +15,7 @@ export default function Hero() {
                         <h1>Hi, I am</h1>
 
                         <h2>Charles</h2>
-                        <h6>Software Engineer/Full Stack Developer/<br />
-                            Data Analyst</h6>
+                        <h6>Software Engineer</h6>
                     </div>
                     <div className="contact-me">
                         {/*<a href={`https://api.whatsapp.com/send/?phone=${process.env.REACT_APP_PHONE_NUMBER}&text&type=phone_number&app_absent=0`} target="_blank" rel="noreferrer"><i className="fa-brands fa-whatsapp"></i></a>*/}
