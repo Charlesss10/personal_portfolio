8d5958615942a9e7944c6b9f827e2844648f040d
Eboson Charles Ikenna <ebosonc@gmail.com>
2026-08-30 13:44:47 +0200

    Commit Description - Update Depedencies - Update Website footer credits - Update Brand name

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -28,7 +28,7 @@ export default function Footer() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            CharlesTech Solutions
+            CharlesTech.AI
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="14"
